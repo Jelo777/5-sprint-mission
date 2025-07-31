30af2a4aca30331f8b72443f7fae421bd3f518f3
Jelo777 <junheef1@gmail.com>
2025-07-31 18:53:39 +0900

    채널, 메세지 파일레포지토리

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -0,0 +1,116 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class FileChannelRepository implements ChannelRepository {
+    private final String DIRECTORY;
+    private final String EXTENSION;
+
+    public FileChannelRepository() {
+        this.DIRECTORY = "CHANNEL";
+        this.EXTENSION = ".ser";
+        Path path = Paths.get(DIRECTORY);
+        if (!path.toFile().exists()) {
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public Channel save(Channel channel) {
+        Path path = Paths.get(DIRECTORY, channel.getId() + EXTENSION);
+        try (FileOutputStream fos = new FileOutputStream(path.toFile());
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(channel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return channel;
+    }
+
+    @Override
+    public Optional<Channel> findById(UUID id) {
+        Path path = Paths.get(DIRECTORY, id + EXTENSION);
+        if (!path.toFile().exists()) {
+            return Optional.empty();
+        }
+        try (FileInputStream fis = new FileInputStream(path.toFile());
+             ObjectInputStream ois = new ObjectInputStream(fis);) {
+            Channel channel = (Channel) ois.readObject();
+            return Optional.of(channel);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Channel> findAll() {
+        Path directory = Paths.get(DIRECTORY);
+        if (Files.exists(directory)) {
+            try {
+                List<Channel> channels = Files.list(directory)
+                        .map(path -> {
+                            try (
+                                    FileInputStream fis = new FileInputStream(path.toFile());
+                                    ObjectInputStream ois = new ObjectInputStream(fis)
+                            ) {
+                                Object data = ois.readObject();
+                                return (Channel) data;
+                            } catch (IOException | ClassNotFoundException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }).toList();
+                return channels;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+
+            }
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public long count() {
+        Path directory = Paths.get(DIRECTORY);
+        if (Files.exists(directory)) {
+            return 0;
+        }
+        try (Stream<Path> stream = Files.list(directory)) {
+            return stream.filter(path -> path.toString().endsWith(EXTENSION)).count();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Path path = Paths.get(DIRECTORY, id + EXTENSION);
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        Path path = Paths.get(DIRECTORY, id + EXTENSION);
+        return Files.exists(path);
+    }
+}
