15448863f47065a1aaf8a0f865694ad1015d7025
Jelo777 <junheef1@gmail.com>
2025-07-31 16:17:32 +0900

    FileUserService

--- src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -4,33 +4,115 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.UserService;
 
 import java.io.*;
-import java.util.List;
-import java.util.UUID;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class FileUserService implements UserService {
+    private final String DIRECTORY;
+    private final String EXTENSION;
+
+    public FileUserService() {
+        this.DIRECTORY = "USER";
+        this.EXTENSION = ".ser";
+        Path path = Paths.get(DIRECTORY);
+        if (!path.toFile().exists()) {
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public User create(String username, String email, String password) {
-        return null;
+        User user = new User(username, email, password);
+        Path path = Paths.get(DIRECTORY, user.getId() + EXTENSION);
+        try (FileOutputStream fos = new FileOutputStream(path.toFile());
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
     @Override
     public User find(UUID userId) {
-        return null;
+        Path path = Paths.get(DIRECTORY, userId + EXTENSION);
+        User user = null;
+        try (FileInputStream fis = new FileInputStream(path.toFile());
+             ObjectInputStream ois = new ObjectInputStream(fis);) {
+            user = (User) ois.readObject();
+        } catch (Exception e) {
+            throw new NoSuchElementException("User not found");
+        }
+        return Optional.ofNullable(user).orElseThrow(() -> new NoSuchElementException("User not found"));
     }
 
     @Override
     public List<User> findAll() {
-        return List.of();
+        Path directory = Paths.get(DIRECTORY);
+        if (Files.exists(directory)) {
+            try {
+                List<User> users = Files.list(directory)
+                        .map(path -> {
+                            try (
+                                    FileInputStream fis = new FileInputStream(path.toFile());
+                                    ObjectInputStream ois = new ObjectInputStream(fis)
+                            ) {
+                                Object data = ois.readObject();
+                                return (User) data;
+                            } catch (IOException | ClassNotFoundException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .toList();
+                return users;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public User update(UUID userId, String newUsername, String newEmail, String newPassword) {
-        return null;
+        Path path = Paths.get(DIRECTORY, userId + EXTENSION);
+        User userNullable = null;
+        if (!path.toFile().exists()) {
+            throw new NoSuchElementException("User not found");
+        }
+        try (FileInputStream fis = new FileInputStream(path.toFile());
+             ObjectInputStream ois = new ObjectInputStream(fis);) {
+            userNullable = (User) ois.readObject();
+        } catch (Exception e) {
+            throw new NoSuchElementException("User not found");
+        }
+
+        User user = Optional.ofNullable(userNullable).orElseThrow(() -> new NoSuchElementException("User not found"));
+        user.update(newUsername, newEmail, newPassword);
+
+        try (FileOutputStream fos = new FileOutputStream(path.toFile());
+             ObjectOutputStream oos = new ObjectOutputStream(fos)
+        ) {
+            oos.writeObject(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return user;
     }
 
     @Override
     public void delete(UUID userId) {
-
+        Path path = Paths.get(DIRECTORY, userId + EXTENSION);
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
